218161ceaa41e2f7afaced4c34ea83dc23f5f0e0
Alex Liebscher <alexliebscher@betterup.co>
2025-07-16 11:21:33 -0400

    remove most account settings

--- client/src/components/Nav/SettingsTabs/Account/Account.tsx
+++ client/src/components/Nav/SettingsTabs/Account/Account.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import DisplayUsernameMessages from './DisplayUsernameMessages';
-import DeleteAccount from './DeleteAccount';
-import Avatar from './Avatar';
-import EnableTwoFactorItem from './TwoFactorAuthentication';
-import BackupCodesItem from './BackupCodesItem';
 import { useAuthContext } from '~/hooks';
 
 function Account() {
@@ -14,24 +10,6 @@ function Account() {
       <div className="pb-3">
         <DisplayUsernameMessages />
       </div>
-      <div className="pb-3">
-        <Avatar />
-      </div>
-      {user?.user?.provider === 'local' && (
-        <>
-          <div className="pb-3">
-            <EnableTwoFactorItem />
-          </div>
-          {user?.user?.twoFactorEnabled && (
-            <div className="pb-3">
-              <BackupCodesItem />
-            </div>
-          )}
-        </>
-      )}
-      {/* <div className="pb-3">
-        <DeleteAccount />
-      </div> */}
     </div>
   );
 }
